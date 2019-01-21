89db238ca91b2308d69f48474ba035d15f55bc90
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-01-20 20:38:21 -0800

    js-2: closures

--- JS-2/callbacks.js
+++ JS-2/callbacks.js
@@ -1,6 +1,6 @@
 // Create a callback function and invoke the function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum", "Gum"];
 
 /* 
   //Given this problem: 
@@ -17,31 +17,47 @@ const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
   });
 */
 
+const logger = param => `Logger callback: ${param}`;
+
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+console.log(getLength(items, logger));
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+console.log(last(items, logger));
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+console.log(sumNums(5, 4, logger));
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+console.log(multiplyNums(3, 5, logger));
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return list.includes(item) ? true : false;
 }
+console.log(contains("Gum", items, logger));
 
 /* STRETCH PROBLEM */
 
-function removeDuplicates(array, cb) {
+/* function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+} */
+function removeDuplicates(array, cb) {
+  return cb([...new Set(array)]);
 }
+console.log(removeDuplicates(items, logger));
--- JS-2/closure.js
+++ JS-2/closure.js
@@ -1,5 +1,11 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+const sayName = name => {
+  return function sayHello(message) {
+    return `${message} I'm ${name}`;
+  };
+};
+console.log(sayName("Gabe")("Sup"));
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
@@ -12,8 +18,26 @@ const counter = () => {
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
+let count = 0;
+
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  return {
+    increment: function() {
+      count++;
+      return count;
+    },
+    decrement: function() {
+      count--;
+      return count;
+    }
+  };
 };
+
+console.log(counterFactory().increment());
+console.log(counterFactory().increment());
+console.log(counterFactory().decrement());
+console.log(counterFactory().decrement());
+console.log(counterFactory().decrement());
